6a610f90d4ce75171ebadd99332defc3ee6f93fa
roushandas02 <roushandas02@gmail.com>
2023-06-09 00:47:19 +0530

    second commit

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -57,7 +57,7 @@ elem.setAttribute('column-pivots',
 '["stock"]');
 elem.setAttribute('row-pivots', "["timestamp"]');
 elem.setAttribute( 'columns', '["top_ask_price"]');
-elem.setAttribute ('aggregates',`
+elem.setAttribute('aggregates',`
 {"stock": "distinct count",
 "top_ask_price": "avg",
 "top_bid_price": "avg",
